5567983335bc2ec3709cd8e51b55796d13bf336c
Mustafa AbdulRazek <mustafa@fci.helwan.edu.eg>
2024-09-02 23:15:45 +0300

    add data science project solution

---
+++ 00-Python/data science project solution/my_text.py
@@ -0,0 +1,16 @@
+text = """
+
+The first thing that stands between you and writing your first, real,
+piece of code, is learning the skill of breaking problems down into
+achievable little actions that a computer can do for you. Of course,
+you and the computer will also need to be speaking a common language,
+but we'll get to that topic in just a bit.
+Now breaking problems down into a number of steps may sound a new
+skill, but its actually something you do every day. Let’s look at an
+example, a simple one: say you wanted to break the activity of fishing
+down into a simple set of instructions that you could hand to a robot,
+who would do your fishing for you. Here’s our first attempt to do that,
+check it out
+
+
+"""
